3a07e52cbdce74f918e9d889897a23381f576540
zhangjikai <zhangjk1993@gmail.com>
2017-02-28 20:46:40 +0800

    update

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cd /e/source/git/github/concurrency-in-java
+git checkout master
+git pull
+git checkout gh-pages
+git pull
+cd /e/source/git/gitbook/zhangjk/concurrency-in-java
+gitbook build
+yes | cp -rf /e/source/git/gitbook/zhangjk/concurrency-in-java/_book/* /e/source/git/github/concurrency-in-java/
+cd /e/source/git/github/concurrency-in-java
+git checkout gh-pages
+git add -A .
+git commit -m "update"
+git push
+git checkout master
+rsync -av --exclude='_book' --exclude='.git' --exclude='node_modules' -exclude='README.md' /e/source/git/gitbook/zhangjk/concurrency-in-java/ /e/source/git/github/concurrency-in-java/
+git add -A .
+git commit -m "update"
+git push
